c508e7603cc41825d93b2344bc127bd6c6dd5217
denis.veklich <denis.veklich@authenticgaming.com>
2021-04-20 08:32:49 +0300

    feat(MH-1): added base component and simple styles

---
+++ src/components/OnOff/index.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+const OnOff = (): ReactElement => {
+  return <div>OnOff</div>;
+};
+
+export { OnOff };
---
+++ src/components/Saved/index.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+const Saved = (): ReactElement => {
+  return <div>Saved</div>;
+};
+
+export { Saved };
